f564d7eb2249d242f932489f5b153ed09e895c3c
Michaela Wilson <+>
2022-03-13 18:51:08 -0600

    commit tree 61ec99a0533b64dc1cb6325385004a4defc5a7be

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    const newCats = [...cats, 'Broom']
+    return ([ 'Milo', 'Otis', 'Garfield', 'Broom'])
+}
+
+function prependCat(){
+    const newcats = ['Arnold', ...cats];
+    return (['Arnold', 'Milo', 'Otis', 'Garfield'])
+}
+
+function removeLastCat(){
+    const newcats = cats.slice(2)
+    return (['Milo', 'Otis'])
+}
+
+function removeFirstCat(){
+    const newcats = cats.slice(0);
+    return (['Otis', 'Garfield'])
+}
